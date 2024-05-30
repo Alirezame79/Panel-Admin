e9dcc8df8b588f73d889ab8b169e2c1d84ea1eeb
Alirezame79 <alirezaghmathenglish@gmail.com>
2024-05-30 16:10:39 +0330

    Add Formik for Login

--- src/features/identity/Login.jsx
+++ src/features/identity/Login.jsx
@@ -1,19 +1,48 @@
-
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 const Login = () => {
 
   return (
-    <form className="bg-slate-300 w-1/3 m-auto p-3 rounded-lg shadow-md flex flex-col">
-      <div className="flex flex-col gap-1 m-3">
-        <label htmlFor="username">Username</label>
-        <input type="text" className="text-lg p-1 rounded" />
-      </div>
-      <div className="flex flex-col gap-1 m-3">
-        <label htmlFor="password">Password</label>
-        <input type="password" className="text-lg p-1 rounded" />
-      </div>
-      <button className='bg-slate-500 hover:bg-slate-400 w-1/3 mx-auto mt-8 mb-2 py-2 rounded font-bold text-lg'>Submit</button>
-    </form>
+    <>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.username) {
+            errors.username = "Username can't be empty.";
+          } else if (values.username.length < 4) {
+            errors.username = "Minimum length is 4.";
+          }
+
+          if (!values.password) {
+            errors.password = "Password can't be empty.";
+          } else if (values.password.length < 4) {
+            errors.password = "Minimum length is 4.";
+          }
+
+          return errors;
+        }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        <Form className="bg-slate-300 w-1/3 m-auto p-3 rounded-lg shadow-md flex flex-col">
+          <div className="flex flex-col gap-1 m-3">
+            <label htmlFor="Email">Username</label>
+            <Field className="text-lg p-1 rounded" type="username" name="username" />
+            <ErrorMessage className="text-red-700" name="username" component="p" />
+          </div>
+          <div className="flex flex-col gap-1 m-3">
+            <label htmlFor="Password">Password</label>
+            <Field className="text-lg p-1 rounded" type="password" name="password" />
+            <ErrorMessage className="text-red-700" name="password" component="p" />
+          </div>
+          <button className='bg-slate-500 hover:bg-slate-400 w-1/3 mx-auto mt-8 mb-2 py-2 rounded font-bold text-lg' type="submit" >
+            Submit
+          </button>
+        </Form>
+      </Formik>
+    </>
   )
 }
 
